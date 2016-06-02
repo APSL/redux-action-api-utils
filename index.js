7e2ca37e4c0afc330a5f9b9586db005f2adb6630
alvaro <me@alvaromb.com>
2016-06-02 15:08:05 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import { generateActionsForFetchWithTypeName } from './lib/ActionUtils'
+import {
+  generateHandlerActionsForType,
+  handleFetchActionsForTypes
+} from './lib/ReducerUtils'
+
+export {
+  generateActionsForFetchWithTypeName,
+  generateHandlerActionsForType,
+  handleFetchActionsForTypes
+}
